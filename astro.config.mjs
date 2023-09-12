d276b6c63f21c1773d9b81f28fceb9da7c4f71ab
Darko Bozhinovski <hello@darko.io>
2023-09-12 12:46:01 +0200

    Adds blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,19 @@ export default defineConfig({
     remotePatterns: [{ protocol: 'https' }],
     domains: ["lh3.googleusercontent.com"]
   },
-  integrations: [tailwind(), mdx(), sitemap(), compress({
+  integrations: [tailwind(), mdx(), sitemap(), icon({
+    include: {
+      mdi: ["cog", "linkedin", "twitter", "facebook", "instagram"],
+      uil: ["map-marker", "envelope"],
+      ic: ["baseline-rocket-launch"],
+      bx: ["bxs-briefcase", "bxs-data", "bxs-window-alt", "bxs-bot", "bxs-file-find"],
+    }
+  }), compress({
     svg: false,
-  }),]
+  }),],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
